9114d97d4fb47f166684d7f51ed621ad9cc599dc
Nicholas Gowan <nicholasjgowan@gmail.com>
2026-03-13 21:10:51 +0800

    Make sidebar truly tab-specific by removing default_path

--- background.js
+++ background.js
@@ -5,7 +5,12 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
-chrome.action.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener(async (tab) => {
+  await chrome.sidePanel.setOptions({
+    tabId: tab.id,
+    path: 'sidepanel.html',
+    enabled: true
+  });
   chrome.sidePanel.open({ tabId: tab.id });
 });
 
